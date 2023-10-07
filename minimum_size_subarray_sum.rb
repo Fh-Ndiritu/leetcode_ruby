6267d2f277313a37cf0bf606ff7ace0ec30bf22c
Fh Ndiritu <fhndiritu@gmail.com>
2023-10-07 19:26:31 +0300

    minimum size subarray

--- minimum_size_subarray_sum.rb
+++ minimum_size_subarray_sum.rb
@@ -24,5 +24,21 @@ end
 
 
 
+def min_sub_array_len(target, nums)
+    min_len = Float::INFINITY
+    prefix_sum = [0]
+    sum = 0
+    prefix_sums = nums.each.with_object([0]) { |num, arr| arr << (arr[-1] + num) }
+    nums.each_with_index{|num, index| prefix_sum << num+prefix_sum[-1]}
+    nums.each_with_index do |num, index|
+        right = prefix_sums.bsearch_index { |p_sum| p_sum - prefix_sums[index] >= target }
+        min_len = [right - index, min_len].min if right
 
-p min_sub_array_len(7, [2, 3, 1, 2, 4, 3])
+    end
+
+    min_len == Float::INFINITY ? 0 : min_len
+    
+end
+
+
+p min_sub_array_len(11, [1,2,3,4,5])
