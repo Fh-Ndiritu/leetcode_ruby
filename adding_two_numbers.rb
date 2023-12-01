f14c406a95ad7c6f22cde02a7798e15991e6fd31
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-01 12:55:15 +0300

    verbose solution to listnodes sum

--- adding_two_numbers.rb
+++ adding_two_numbers.rb
@@ -10,8 +10,8 @@ l13 = ListNode.new(3)
 l12 = ListNode.new(4, l13)
 l1 = ListNode.new(2, l12)
 
-l23 = ListNode.new(4)
-l22 = ListNode.new(6, l23)
+# l23 = ListNode.new(4)
+l22 = ListNode.new(6)
 l2 = ListNode.new(5, l22)
 
 
@@ -19,37 +19,48 @@ l2 = ListNode.new(5, l22)
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
-    p l1.size
-    exit;
+
     a1, a2 = [], []
+    i = 0 
+
     n1s = l1
-    while n1s
-        a1 << n1s.val
-        n1s = n1s.next
-    end
-
     n2s = l2
-    while n2s
-        a2 << n2s.val
+    while n1s || n2s
+        if n1s
+            a1 << n1s.val
+            n1s = n1s.next
+        end
+        if n2s
+         a2 << n2s.val
         n2s = n2s.next
+        end
+
     end
 
-    a2 =  a2.reverse.join.to_i
-    a1=  a1.reverse.join.to_i
-    node_sum= (a2+a1).to_s.chars.reverse
-    parent = ListNode.new(node_sum[0])
-    head = parent
-
-    i = 1
-    while i < node_sum.size
-        val = node_sum[i]
-        n = ListNode.new(val)
-        parent.next = n 
-        parent = n
-        i += 1
+    if a1.size >= a2.size 
+        base_node = l1
+        filler = [0]*(a1.size-a2.size)
+        short_arr = filler.concat(a2)
+        long_arr = a1
+    else
+        base_node = l2
+        filler = [0]*(a2.size-a1.size)
+        short_arr =[0]*(a2.size-a1.size).concat(a1)
+        long_arr = a2
     end
 
-     head
+    last_node = ListNode.new()
+    head = last_node
+
+    i = long_arr.size
+    while i >= 0
+        p long_arr[i]
+        p short_arr[i]
+        # n = ListNode.new(val)
+        # parent.next = n 
+        # parent = n
+        i -= 1
+    end
 
 end
 
