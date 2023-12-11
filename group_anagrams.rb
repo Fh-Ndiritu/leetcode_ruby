690ba05257c0ac48a919e49654d52e808bab7486
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-11 13:19:04 +0300

    adding a nice one liner

--- group_anagrams.rb
+++ group_anagrams.rb
@@ -13,6 +13,12 @@ def group_anagrams(strs)
 end
 
 
+# https://leetcode.com/problems/group-anagrams/solutions/4390145/ruby-oneliner-explained/
+
+def group_anagrams(strs)
+    strs.each.with_object({}){|str, res|  (res[str.chars.sort.join] ||= []) << str}.values
+end
+
 strs = ["eat","tea","tan","ate","nat","bat"]
 # strs = [""]
 # strs = ["a"]
