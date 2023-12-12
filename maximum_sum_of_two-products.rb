77ff997422e609578ba668006927b255fd19c5dc
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-12 10:32:36 +0300

    oneliner max & inject solution

--- maximum_sum_of_two-products.rb
+++ maximum_sum_of_two-products.rb
@@ -5,6 +5,10 @@ def max_product(nums)
     return (nums[-1]-1) * (nums[-2] - 1)
 end
 
+def max_product(nums)
+    nums.max(2).inject(1){|res, num| res*(num-1)}
+end
+
 p max_product([3, 4, 5, 2])
 p max_product([1, 5, 4, 5])
 p max_product([1, 1])
