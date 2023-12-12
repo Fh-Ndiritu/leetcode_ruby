2e0b6a6884948d02285abe8111efedf99c711377
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-12 10:28:30 +0300

    solution with sort

---
+++ maximum_sum_of_two-products.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def max_product(nums)
+    nums.sort!
+    return (nums[-1]-1) * (nums[-2] - 1)
+end
+
+p max_product([3, 4, 5, 2])
+p max_product([1, 5, 4, 5])
+p max_product([1, 1])
