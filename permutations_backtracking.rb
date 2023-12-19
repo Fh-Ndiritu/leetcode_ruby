413f915ee5e11656a14267c97e8c29d80902717c
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-19 14:20:31 +0300

    backtracking method

--- permutations_backtracking.rb
+++ permutations_backtracking.rb
@@ -1,7 +1,29 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def permute(nums)
-    nums.permutation(nums.size).to_a
+
+    def perm(comb, nums)
+        return [comb.dup] if nums.empty?
+
+        combs = []
+        nums.each_with_index do |num, i|
+            comb << num 
+            combs += perm(comb, nums[0...i]+nums[i+1..])
+            comb.pop
+        end
+
+        return combs
+    end
+
+    perms = []
+
+    nums.each_with_index do |num, i|
+        perms += perm([num], nums[0...i] + nums[i+1..])
+    end
+    
+    perms
+
 end
+
 
 p permute([1,2,3])
