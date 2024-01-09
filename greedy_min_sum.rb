b346c1956dfc97f0a8c5fb1ba3e340b50e91116a
Fh Ndiritu <fhndiritu@gmail.com>
2024-01-09 16:11:18 +0300

    interesting

--- greedy_min_sum.rb
+++ greedy_min_sum.rb
@@ -1,10 +1,10 @@
 # @param {Integer} num
 # @return {Integer}
 def minimum_sum(num)
-    nums = num.to_s.chars
-    min = nums.min(2)
-    max = nums.max(2)
-    (min[0]+max[1]).to_i + (min[1]+max[0]).to_i
+    nums = num.to_s.chars.sort
+    one = nums[0]+nums[2]
+    two = nums[1]+nums[3]
+    one.to_i+two.to_i
 end
 
 p minimum_sum(2932)
