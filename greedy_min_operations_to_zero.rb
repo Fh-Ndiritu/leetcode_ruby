fd9a77231bed0bcd46d11af6ad51ff9981f4971a
Fh Ndiritu <fhndiritu@gmail.com>
2024-01-09 17:12:43 +0300

    verbose with no recursion

--- greedy_min_operations_to_zero.rb
+++ greedy_min_operations_to_zero.rb
@@ -1,21 +1,24 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimum_operations(nums)
+    nums = nums.uniq.select{|i| i > 0}.sort
+    return 0 unless !nums.empty?
 
-    def reduce(ops, arr)
-        # selected = arr.reject{|i| i <1}.min
-        selected = arr.select{|i| i > 0 }.min
-        return ops unless selected
+    diffed = 0 
+    max = nums.last 
 
-        ops += 1
-        arr.map!{|n| n - selected }
-        reduce(ops, arr)
-
+    i = 0 
+    while max > 0 
+        val = nums[i]-diffed
+        max -= val
+        diffed = nums[i]
+        i += 1
     end
 
-    reduce(0, nums)
+    return i 
     
 end
-
-# p minimum_operations([0, 0 ])
+p minimum_operations([1,3,2,2,3,4,1,1,4])
+p minimum_operations([0, 0 ])
 p minimum_operations([1,5,0,3,5])
+p minimum_operations([1, 2, 3, 4, 5])
