fc05980ab286d5ed437635032335a79790ec5b8f
Fh Ndiritu <fhndiritu@gmail.com>
2024-01-12 12:20:49 +0300

    less memory sol

--- eq_string_half.rb
+++ eq_string_half.rb
@@ -2,24 +2,27 @@
 # @return {Boolean}
 def halves_are_alike(s)
     vowels = {'a'=>1, 'e'=>1, 'i'=>1, 'o'=>1, 'u'=>1, 'A'=>1, 'E'=>1, 'I'=>1, 'O'=>1, 'U'=>1}
-    midpoint = s.size/2
+    len =s.size 
+    midpoint = len/2
 
-    first_v = 0 
-    s[0...midpoint].chars.each do |char|
-        if vowels[char]
-            first_v += 1
+
+    i = count = 0 
+    while i < midpoint
+        first_char = s[i]
+        if vowels[first_char]
+            count += 1
         end
+
+        second_char = s[len-1-i]
+        if vowels[second_char]
+            count -= 1
+        end
+        i += 1
+
     end
 
-    second_v = 0 
-    s[midpoint..].chars.each do |char|
-        if vowels[char]
-            second_v += 1
-        end
-    end
-
-    return second_v == first_v
+    return count==0
     
 end
 
-p halves_are_alike("os")
+p halves_are_alike("bOok")
